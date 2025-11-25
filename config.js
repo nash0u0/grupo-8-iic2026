09c1cd1f9b58e0f1b30f8b30343b85e9f65bc217
nash0u0 <ignacio.schatzke@uc.cl>
2025-11-25 18:53:45 -0300

    Fisicalizacion

---
+++ config.js
@@ -0,0 +1,13 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+	[
+		{ 
+			name: "Servo",
+			page: "arduino.html",
+		},
+		{ 
+			name: "Map",
+			page: "prueba4.html",
+			main: true,
+		}
+	]);
